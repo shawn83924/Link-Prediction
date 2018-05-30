01b2f60940b1761c680c3b67f25779cb9a6f024c
shawn83924 <shawn83924@gmail.com>
2018-05-31 01:51:38 +0800

    finish matrix

--- linkPrediction.py
+++ linkPrediction.py
@@ -6,12 +6,10 @@ This is a temporary script file.
 """
 import csv
 import numpy as np
-import pandas as pd
-import collections
 
 vertex_list = []
 
-
+#刪除陣列中重複數值的函示
 def remove_duplicates(values):
     output = []
     seen = set()
@@ -35,9 +33,9 @@ with open('Period1.csv', newline='') as csvfile:
         #將第二段與第三段加進list
         vertex_list.extend([string[1],string[2]])
     
-    #將陣列值進行處理，排序後將重複的數值刪除，留下唯一值
-    sorted(vertex_list)
+    #將陣列值進行處理，將重複的數值刪除後排序，留下唯一值
     vertex_list = remove_duplicates(vertex_list)
+    sorted(vertex_list)
     #測試陣列長度
     print(len(vertex_list))
     
@@ -51,27 +49,48 @@ with open('Period2.csv', newline='') as csvfile:
         #將第二段與第三段加進list
         vertex_list.extend([string[1],string[2]])
     
-    #將陣列值進行處理，排序後將重複的數值刪除，留下唯一值
-    
+    #將陣列值進行處理，將重複的數值刪除後排序，留下唯一值
     vertex_list = remove_duplicates(vertex_list)
     sorted(vertex_list)
     #測試陣列長度
     print(len(vertex_list))      
     
-    """測試有無重複值
-    for content1 in vertex_list:
-        count=0
-        for content2 in vertex_list:
-            if content1==content2:
-                count+=1
-            if count>=2:
-                print(content1)
-    """           
     
+#宣告NxN的陣列，且內容均為0
+n=len(vertex_list)      
+Matrix = np.full( ( n, n ),0 )
+
+#開始填值，將Period2 的 link 以1填入
+print("start Period2")
+count=0
+with open('Period2.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        #一行行切成3段
+        string =', '.join(row).split(",",2)
+        i= vertex_list.index(string[1])
+        j= vertex_list.index(string[2])
+        Matrix[i][j] = 1
+        count+=1
+#98353
+print(count)        
+print("done")
     
-    
-    
-    
+#開始填值，將Period1 的 link 以2填入
+print("start Period1")
+count=0
+with open('Period1.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        #一行行切成3段
+        string =', '.join(row).split(",",2)
+        i= vertex_list.index(string[1])
+        j= vertex_list.index(string[2])
+        Matrix[i][j] = 2
+        count+=1
+#154836
+print(count)        
+print("done")    
     
     
     
